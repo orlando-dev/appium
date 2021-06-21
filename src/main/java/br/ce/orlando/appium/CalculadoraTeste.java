97c24728d8f01b949c366246be02a9d1b423337d
Orlando <orlando.junior07@hotmail.com>
2021-06-21 15:33:10 -0300

    Criando projeto maven e primeiro teste mobile.

---
+++ src/main/java/br/ce/orlando/appium/CalculadoraTeste.java
@@ -0,0 +1,36 @@
+package br.ce.orlando.appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class CalculadoraTeste {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("platformName", "Android");
+		desiredCapabilities.setCapability("deviceName", "emulator-5554");
+		desiredCapabilities.setCapability("automationName", "uiautomator2");
+		desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+		desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+		
+		
+		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+		
+		MobileElement el1 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_1");
+	    el1.click();
+	    MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("plus");
+	    el2.click();
+	    MobileElement el3 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_1");
+	    el3.click();
+	    MobileElement el4 = (MobileElement) driver.findElementByAccessibilityId("equals");
+	    el4.click();
+	    
+	    driver.quit();
+	}
+}
