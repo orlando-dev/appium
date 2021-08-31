4cb3250c0eea6b61d7e445122c8b5d5da80afb91
Orlando <orlando.junior07@hotmail.com>
2021-08-31 18:21:28 -0300

    Uso do resetApp para melhor eficiencia entre os testes

--- src/main/java/br/ce/orlando/appium/core/BaseTest.java
+++ src/main/java/br/ce/orlando/appium/core/BaseTest.java
@@ -1,12 +1,18 @@
 package br.ce.orlando.appium.core;
 
 import org.junit.After;
+import org.junit.AfterClass;
 
 public class BaseTest {
 	
 	
+	@AfterClass
+	public static void finalizaClasse() {
+		DriverFactory.killDriver();
+	}
+	
 	@After
 	public void tearDown() {
-		DriverFactory.killDriver();
+		DriverFactory.getDriver().resetApp();
 	}
 }
--- src/main/java/br/ce/orlando/appium/page/MenuPage.java
+++ src/main/java/br/ce/orlando/appium/page/MenuPage.java
@@ -1,13 +1,6 @@
 package br.ce.orlando.appium.page;
 
-import java.net.MalformedURLException;
-
-import org.junit.After;
-import org.junit.Before;
-
 import br.ce.orlando.appium.core.BasePage;
-import br.ce.orlando.appium.core.DSL;
-import br.ce.orlando.appium.core.DriverFactory;
 
 public class MenuPage extends BasePage{
 	
